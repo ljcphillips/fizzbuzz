71cf6f15c37d2dcc8860b3ca30aa419a9f626089
Andrew <akemp850@gmail.com>
2018-03-14 14:27:30 +0000

    added fizz test

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,5 @@
+class Integer
+  def fizzbuzz
+    "fizz"
+  end
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,7 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+end
